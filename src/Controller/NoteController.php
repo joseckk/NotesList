1fa6b6335b659249ab2fadc684d8ba14485bac88
joseckk <josemethvin@gmail.com>
2021-12-20 13:06:48 +0100

    Creando base de datos local y CRUD de Note

--- src/Controller/NoteController.php
+++ src/Controller/NoteController.php
@@ -2,19 +2,90 @@
 
 namespace App\Controller;
 
+use App\Entity\Note;
+use App\Form\NoteType;
+use App\Repository\NoteRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class NoteController extends AbstractController
 {
     /**
-     * @Route("/note", name="note")
+     * @Route("/", name="app_list_note")
      */
-    public function index(): Response
+    public function list(NoteRepository $notesRepository): Response
     {
-        return $this->render('note/index.html.twig', [
-            'controller_name' => 'NoteController',
+        $notes = $notesRepository->findAll();
+        return $this->render('note/list.html.twig', [
+            'notes' => $notes,
         ]);
+    }
+
+    /**
+     * @Route("/new-note", name="app_new_note")
+     */
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $note = new Note();
+        $form = $this->createForm(NoteType::class, $note);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($note);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('/', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('note/new.html.twig', [
+            'note' => $note,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="note_show", methods={"GET"})
+     */
+    public function show(Note $note): Response
+    {
+        return $this->render('note/show.html.twig', [
+            'note' => $note,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="note_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Note $note, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(NoteType::class, $note);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('note_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('note/edit.html.twig', [
+            'note' => $note,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="note_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Note $note, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$note->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($note);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('note_index', [], Response::HTTP_SEE_OTHER);
     }
 }
